c0bf2b3128756854d6b70609708453a575dcecac
Sandor Kazi <sandor.kazi@gmail.com>
2020-10-21 09:02:44 +0200

    contest button changed

--- bgg_contest.gm.js
+++ bgg_contest.gm.js
@@ -1,101 +1,101 @@
 "use strict";
 // ==UserScript==
-// @name         bgg contest
-// @namespace    bgg
-// @version      1.1
-// @description  bgg contest autofill
-// @match        https://*.boardgamegeek.com/thread/*
-// @match        https://boardgamegeek.com/thread/*
-// @author       masu
-// @grant        GM_addStyle
+// @name        bgg contest
+// @namespace   bgg
+// @version     1.1
+// @description bgg contest autofill
+// @match       https://*.boardgamegeek.com/thread/*
+// @match       https://boardgamegeek.com/thread/*
+// @author      masu
+// @grant       GM_addStyle
 // ==/UserScript==
 
 function parseAnswer(radioButton) {
-    let text = radioButton.nextSibling.textContent.trim();
-    // noinspection JSValidateTypes
-    text = text.split(')');
-    return {
-        'radio': radioButton,
-        'id': text.shift().trim(),
-        'text': text.join(')').trim(),
-    }
+  let text = radioButton.nextSibling.textContent.trim();
+  // noinspection JSValidateTypes
+  text = text.split(')');
+  return {
+    'radio': radioButton,
+    'id': text.shift().trim(),
+    'text': text.join(')').trim(),
+  }
 }
 
 function parseQuestion(box) {
-    let question = box.firstChild.textContent.trim();
-    let answers = Array.from(box.querySelectorAll('input')).map(parseAnswer);
-    // noinspection JSValidateTypes
-    question = question.split(':');
-    return {
-        'question_id': question.shift().trim(),
-        'question': question.join(':').trim(),
-        'answers': answers,
-    }
+  let question = box.firstChild.textContent.trim();
+  let answers = Array.from(box.querySelectorAll('input')).map(parseAnswer);
+  // noinspection JSValidateTypes
+  question = question.split(':');
+  return {
+    'question_id': question.shift().trim(),
+    'question': question.join(':').trim(),
+    'answers': answers,
+  }
 }
 
 function parseQuestions(response) {
-    let page = document.querySelector('#autofill_view');
-    // noinspection InnerHTMLJS
-    page.innerHTML = response;
-    let boxes = Array.from(page.querySelectorAll('.question'));
-    return {
-        'questions': boxes.slice(1).map(parseQuestion),
-        'submit': page.querySelector('input[type="submit"]'),
-        'links': Array.from(boxes[0].querySelectorAll('a')).map((a) => (a.href)),
-    };
+  let page = document.querySelector('#autofill_view');
+  // noinspection InnerHTMLJS
+  page.innerHTML = response;
+  let boxes = Array.from(page.querySelectorAll('.question'));
+  return {
+    'questions': boxes.slice(1).map(parseQuestion),
+    'submit': page.querySelector('input[type="submit"]'),
+    'links': Array.from(boxes[0].querySelectorAll('a')).map((a) => (a.href)),
+  };
 }
 
 function autofill(url) {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.setRequestHeader('Content-type', 'text/html');
-    xhr.onload = function() {
-        let form = parseQuestions(xhr.response);
-        randomFill(form);
-    };
-    xhr.send();
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.setRequestHeader('Content-type', 'text/html');
+  xhr.onload = function() {
+    let form = parseQuestions(xhr.response);
+    randomFill(form);
+  };
+  xhr.send();
 }
 
 function randomFill(form) {
-    form.questions.map(
-        (question) => (
-            question.answers[Math.floor(Math.random()*question.answers.length)].radio.click()
-        )
-    );
-    form.submit.click();
+  form.questions.map(
+    (question) => (
+      question.answers[Math.floor(Math.random()*question.answers.length)].radio.click()
+    )
+  );
+  form.submit.click();
 }
 
 function append() {
-    let aContest = document.querySelector('a[href="/user/BGG Contest"]');
-    if (null == aContest) {
-        aContest = document.querySelector('a[href="/user/chaddyboy_2000"]');
-        if (null == aContest) {
-            return;
-        }
-    }
-    let as = (
-        aContest.parentElement.parentElement.parentElement.nextElementSibling
-    ).querySelectorAll('a[class="postlink"][target="_blank"][rel="nofollow"]');
-    let aEnter = as[as.length - 1];
-    if (null == aEnter) {
-        return;
-    }
-    let aAutofill = document.querySelector('#autofill');
-    if (null == aAutofill) {
-        aAutofill = document.createElement('a');
-        aAutofill.className = 'postlink';
-        aAutofill.text = 'Autofill Now!';
-        aAutofill.id = 'autofill';
-        let divAutofill = document.createElement('div');
-        divAutofill.id = 'autofill_view';
-        aEnter.parentElement.appendChild(document.createElement('br'));
-        aEnter.parentElement.appendChild(aAutofill);
-        aEnter.parentElement.appendChild(document.createElement('br'));
-        aEnter.parentElement.appendChild(divAutofill);
-    }
-    aAutofill.onclick = function() {
-        autofill(aEnter.href + '/questions');
-    };
+  let aContest = null;
+  let aAutofill = null; 
+  var checkExist = setInterval(function() {
+   aContest = document.querySelector("a[href*=\"https://www.boardgamegeek.com/contest/\"]");
+   if (aContest != null) {
+    console.log("Found a contest link...");
+    var checkExist2 = setInterval(function() {
+     aAutofill = document.querySelector('#autofill');
+     if (null == aAutofill) {
+       console.log("Adding autofill link...");
+       aAutofill = document.createElement('a');
+       aAutofill.className = 'postlink';
+       aAutofill.text = 'Autofill Now!';
+       aAutofill.id = 'autofill';
+       let divAutofill = document.createElement('div');
+       divAutofill.id = 'autofill_view';
+       aContest.parentElement.appendChild(document.createElement('br'));
+       aContest.parentElement.appendChild(aAutofill);
+       aContest.parentElement.appendChild(document.createElement('br'));
+       aContest.parentElement.appendChild(divAutofill);
+       aAutofill.onclick = function() {
+         autofill(aContest.href + '/questions');
+       };
+     } else {
+      { clearInterval(checkExist2) }
+     }
+    }, 100)
+    clearInterval(checkExist);
+   }
+  }, 100);
 }
 
 append();
